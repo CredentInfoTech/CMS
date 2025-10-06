2417c796ab90dee9eab3e86993081f3bcd783f26
Akashdeep singh <singh483862@gmail.com>
2025-10-06 12:10:37 +0530

    resolve all warnings

--- src/webparts/cmsRebuild/components/CMSPages/Loader.tsx
+++ src/webparts/cmsRebuild/components/CMSPages/Loader.tsx
@@ -6,7 +6,9 @@ import "./Loader.css"; // Ensure you create and import this CSS file
 const CISLoader = () => {
   return (
     <div className="loader-container">
-      <img src={require("../../assets/loader.gif")} alt="Loading..." className="loader-gif" />
+      {/* <img src={require("../../assets/loader.gif")} alt="Loading..." className="loader-gif" /> */}
+      {/* <img src={require("../../assets/loader.gif")} alt="Loading..." className="loader-gif" /> */}
+      <img src="https://assets-v2.lottiefiles.com/a/d5392796-1169-11ee-908e-b33ed8d96ca4/kW0SJwvz27.gif" alt="Loading..." className="loader-gif" />
       {/* <div className="loader-text">CIS</div> */}
     </div>
   );
