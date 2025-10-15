7da7fb059dc292a929f06ef21bcb77dc3f345074
AkashdeepSingh-CIS <akashdeep.singh@credentinfotech.com>
2025-10-15 10:53:08 +0530

    Update ListDetail.tsx

--- src/webparts/cmsRebuild/components/CMSPages/ListDetail.tsx
+++ src/webparts/cmsRebuild/components/CMSPages/ListDetail.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
+/* eslint-disable @typescript-eslint/no-use-before-define*/
+/* eslint-disable  @typescript-eslint/no-floating-promises*/
+/* eslint-disable  @typescript-eslint/explicit-function-return-type */
 import * as React from "react";
 import { useState, useEffect, useRef } from "react";
 import { DataGrid, GridColDef, GridRenderCellParams, GridPaginationModel, GridToolbar } from "@mui/x-data-grid";
@@ -484,4 +486,4 @@ export default function ListDetail({
             </div>
         </div>
     );
-
+}
