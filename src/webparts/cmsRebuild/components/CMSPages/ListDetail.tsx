68fb435899db4f52d1cc1046ca171a7bd4a5f785
Akashdeep singh <singh483862@gmail.com>
2025-10-15 11:44:27 +0530

    latest code

--- src/webparts/cmsRebuild/components/CMSPages/ListDetail.tsx
+++ src/webparts/cmsRebuild/components/CMSPages/ListDetail.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-use-before-define */
-/* eslint-disable  @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-use-before-define*/
+/* eslint-disable  @typescript-eslint/no-floating-promises*/
 /* eslint-disable  @typescript-eslint/explicit-function-return-type */
+
 import * as React from "react";
 import { useState, useEffect, useRef } from "react";
 import { DataGrid, GridColDef, GridRenderCellParams, GridPaginationModel, GridToolbar } from "@mui/x-data-grid";
